ded68e33b5eee4398974ca839876b88f3dfba90d
Rezowan Rahman <reznish2000@gmail.com>
2014-06-01 16:41:08 +0600

    pipeline module form work

--- src/Venture/PipeLineBundle/Controller/DefaultController.php
+++ src/Venture/PipeLineBundle/Controller/DefaultController.php
@@ -3,26 +3,49 @@
 namespace Venture\PipeLineBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/list{type}" , name="venture_pipeline_list")
+     * @Method("GET")
      * @Template()
      */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
+    public function indexAction($type="show_all") {
+        return array();
     }
-    
-    /*
-    public function addAction() {
-        
+
+    /**
+     * @Route("/add", name="venture_pipeline_add")
+     * @Method("GET|POST")
+     * @Template()
+     */
+    public function addAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $pipeLine = new \Venture\PipeLineBundle\Entity\PipeLine();
+
+        $form = $this->createForm(new \Venture\PipeLineBundle\Form\PipeLineType(), $pipeLine);
+
+        if ($request->getMethod() == 'POST') {
+
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+
+                $em->persist($pipeLine);
+                $em->flush();
+
+                $this->get('session')->getFlashBag()->add('notice','A Pipeline entry is eneterd into the database');
+                #return $this->redirect($this->generateUrl("venture_show_alternate_raw_material", array("id" => $alternateRawMaterial->getId())));
+            }
+        }
+
+        return array('form' => $form->createView());
     }
-    
-    public function getForm() {
-        
-    }*/
 }
